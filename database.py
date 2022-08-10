2094ca2dab9aba7e9b0f83b746fbabe0efea172f
NovikovNik <nowickow.kol@yandex.ru>
2022-08-10 14:20:19 +0300

    Подключение к базе теперь идет через session maker, не нужно вручную закрывать соединение с базой

--- database.py
+++ database.py
@@ -1,13 +1,12 @@
-from sqlite3 import threadsafety
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 engine = create_engine("sqlite:///./memory.db", echo=True)
-session = sessionmaker(bind=engine, autocommit=False, autoflush=False, threadsafety = False)
+session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 
 def get_db():
-    ses = Session(engine)
+    ses = session
     return ses
--- user.py
+++ user.py
@@ -2,33 +2,34 @@ from datetime import datetime
 from database import get_db
 import models
 
-def initial_user_create(user_name: str, nick: str, chat_id: id):
-    db = get_db()
+db = get_db()
+
+def initial_user_create(user_name: str, nick: str, chat_id: id) -> None:
     new_user = models.User(name=user_name, 
                            first_authorization=datetime.now(), 
                            nick_name=nick, 
                            chat_id=chat_id)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    db.close()
+    with db.begin() as session:
+        session.add(new_user)
+        session.commit()
+        session.refresh(new_user)
+    
     
 def find_user_in_db(user_id: int) -> models.User:
-    db = get_db()
-    user = db.query(models.User).filter(models.User.name == user_id).first()
-    db.close()
-    return user
+    with db.begin() as session:
+        user = session.query(models.User).filter(models.User.name == user_id).first()
+        return user
 
 
 def delete_user_from_db(user_id: int) -> None:
-    db = get_db()
-    db.query(models.User).filter(models.User.name == user_id).delete()
-    db.commit()
-    db.close()
+    with db.begin() as session:
+        session.query(models.User).filter(models.User.name == user_id).delete()
+        session.commit()
+        
 
 def get_all_chat_ids() -> int:
     """Генератор возвращающий id чата от каждого пользователя
     """
-    db = get_db()
-    for id in db.query(models.User.chat_id).distinct():
-        yield(id[0])
+    with db.begin() as session:
+        for id in session.query(models.User.chat_id).distinct():
+            yield(id[0])
