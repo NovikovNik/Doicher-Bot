5fcf900f96e404847aed62c3f5319a944da083cb
Nikolay Novikov <83706296+NovikovNik@users.noreply.github.com>
2022-10-06 13:23:02 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bot_body import start_pooling
 
 
 if __name__ == "__main__":
-    if debug := False == True:
+    if debug := True == True:
         Base.metadata.drop_all(engine)  # Убрать позже
     Base.metadata.create_all(engine)
     database = get_db()
